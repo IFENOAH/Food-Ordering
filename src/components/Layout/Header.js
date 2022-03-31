93d1033ac2bfc90fa2a2bc4d58651ca0e25a366f
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2022-03-31 20:30:58 +0100

    adding the cart Button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from "react";
 import image1 from "../../assets/meals.jpeg";
 import styles from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={styles.header}>
         <h1>Noah's</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={styles["main-image"]}>
         <img src={image1} alt="table full of meals" />
