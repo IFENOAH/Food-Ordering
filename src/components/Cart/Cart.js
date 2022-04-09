ea7b1955e6b79162cb55a92ed71c21a4393a6e63
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2022-04-09 23:03:06 +0100

    adding amount in cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,10 +9,10 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
   const cartItemRemoveHandler = (id) => {
-    cartCtx.removeItem(id);
+    // cartCtx.removeItem(id);
   };
-  const cartItemAddHandler = (item) => {
-    cartCtx.addItem(item);
+  const cartItemAddHandler = (item, amount) => {
+    cartCtx.addItem({ ...item, amount: 1 });
   };
   const cartItems = (
     <ul className={styles["cart-items"]}>
@@ -23,7 +23,7 @@ const Cart = (props) => {
           amount={item.amount}
           price={item.price}
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAdd={cartItemAddHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
         />
       ))}
     </ul>
